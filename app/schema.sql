55bebd9fdad3aee20bbd867375d9fe019944e8f1
FurtherLee <95588lsj@gmail.com>
2012-05-29 20:52:42 +0800

    finish schema

--- app/schema.sql
+++ app/schema.sql
@@ -9,10 +9,58 @@ CREATE TABLE IF NOT EXISTS `users` (
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `salt` varchar(80) NOT NULL,
   `iter` int(11) NOT NULL,
+  `photo` varchar(200),
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
-INSERT INTO `users` (`id`, `email`, `name`, `sex`, `password`, `create_time`, `salt`, `iter`, `birthday`) VALUES
-(1, 'test@paike.com', 'test', '男', 'c23fd1ab5aa572349eb30e15d0437ee3', '2012-05-29 10:39:41', '4a37a7266e9a19cbe63e7ce5bca07ebc', 9, '2012-05-29');
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `desp` text NOT NULL,
+  `leader` int(11) NOT NULL,
+  `photo` varchar(200), 
+  KEY `leader` (`leader`),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`leader`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+CREATE TABLE IF NOT EXISTS `works` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `desp` text NOT NULL,
+  `gid` int(11) NOT NULL,
+  KEY `gid` (`gid`),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`gid`) REFERENCES `groups` (`id`)   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `workers` (
+  `uid` int(11) NOT NULL,
+  `wid` int(11) NOT NULL,
+  PRIMARY KEY(`uid`, `wid`),
+  KEY `uid` (`uid`),
+  KEY `wid` (`wid`),
+  FOREIGN KEY(`uid`) REFERENCES `users` (`id`),
+  FOREIGN KEY(`wid`) REFERENCES `works` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `schedules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `desp` text NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `gid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `gid` (`gid`),
+  FOREIGN KEY(`gid`) REFERENCES `groups` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `assigns` (
+  `sid` int(11) NOT NULL,
+  `wid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  FOREIGN KEY(`uid`) REFERENCES `users` (`id`),
+  FOREIGN KEY(`wid`) REFERENCES `works` (`id`),
+  FOREIGN KEY(`sid`) REFERENCES `schedules` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
